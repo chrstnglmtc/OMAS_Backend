ba3c29c47eccb9a7552548930d12c13bd7e4761e
Christine <tinegulmatico@gmail.com>
2024-04-04 14:09:58 +0800

    Added TimetableService

--- omas/src/main/java/com/omasystem/omas/Controller/TimetableController.java
+++ omas/src/main/java/com/omasystem/omas/Controller/TimetableController.java
@@ -3,22 +3,24 @@ package com.omasystem.omas.Controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.omasystem.omas.Dao.TimetableDao;
 import com.omasystem.omas.Model.TimetableModel;
+import com.omasystem.omas.Service.TimetableService;
 
 @RestController
 @RequestMapping("/api/timetable")
 public class TimetableController {
 
-    private final TimetableDao timetableDao;
-
-    public TimetableController(TimetableDao timetableDao) {
-        this.timetableDao = timetableDao;
+    @Autowired
+    private final TimetableService timetableService;
+    
+    public TimetableController(TimetableService timetableService) {
+        this.timetableService = timetableService;
     }
 
     @GetMapping("/reservations")
@@ -26,6 +28,6 @@ public class TimetableController {
         
         Map<String, String> params = Map.of("start_date", startDate, "end_date", endDate);
 
-        return timetableDao.getAllReservationPerTimeslot(params);
+        return timetableService.getAllReservationPerTimeslot(params);
     }
 }
--- omas/src/main/java/com/omasystem/omas/OmasApplication.java
+++ omas/src/main/java/com/omasystem/omas/OmasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.omasystem.omas.Dao")
+@MapperScan("com.omasystem.omas.dao")
 public class OmasApplication {
 
 	public static void main(String[] args) {
